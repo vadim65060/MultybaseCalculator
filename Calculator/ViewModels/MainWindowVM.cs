35fea04c52fe6424790efa73998679b0c2453c62
vadim65060 <vadim65060@mail.ru>
2024-04-12 22:10:35 +0300

    off button, if cant print

--- Calculator/ViewModels/MainWindowVM.cs
+++ Calculator/ViewModels/MainWindowVM.cs
@@ -12,14 +12,32 @@ namespace Сalculator.ViewModels
         public DelegateCommand EraseCommand { get; }
         public string Equation => _model.Equation;
 
-        public int CountOpenBrackets => _model.CountOpenBrackets;
-
-
         public int Base
         {
             get => _model.Base;
-            set => _model.Base = value;
+            set
+            {
+                _model.Base = value;
+                UpdateButtons();
+            }
         }
+
+        public bool CanPrintMinus =>
+            Equation.Length == 0 || !Operators.Contains(Equation[Equation.Length - 1].ToString());
+
+        public bool CanPrintDot => !CantPrintDot();
+
+        public bool CanPrintOperator => !(Equation.Length == 0 ||
+                                          Equation[Equation.Length - 1] == '(' ||
+                                          Operators.Contains(Equation[Equation.Length - 1].ToString()) ||
+                                          Equation.Contains("NaN"));
+
+        public bool CanPrintCloseBracket =>
+            _model.CountOpenBrackets > 0 && Equation[Equation.Length - 1] >= '0' &&
+            Equation[Equation.Length - 1] <= '9';
+
+        public bool CanPrintOpenBracket => Equation.Length == 0 || Equation[Equation.Length - 1] != ',';
+
 
         private const string Operators = "+-*/,";
         private readonly CalculatorModel _model = new CalculatorModel();
@@ -27,60 +45,60 @@ namespace Сalculator.ViewModels
         public MainWindowVM()
         {
             _model.PropertyChanged += (s, e) => { RaisePropertyChanged(e.PropertyName); };
-            CalculateCommand = new DelegateCommand(() => { _model.Calculate(); });
+            CalculateCommand = new DelegateCommand(() =>
+            {
+                _model.Calculate();
+                UpdateButtons();
+            });
             AddCommand = new DelegateCommand<string>(input =>
             {
-                if (WrongInputValidation(input))
-                {
-                    return;
-                }
-
                 _model.AddToEquation(input);
+                UpdateButtons();
             });
-            ClearCommand = new DelegateCommand(() => { _model.ClearEquation(); });
-            EraseCommand = new DelegateCommand(() => { _model.EraseEquation(); });
+            ClearCommand = new DelegateCommand(() =>
+            {
+                _model.ClearEquation();
+                UpdateButtons();
+            });
+            EraseCommand = new DelegateCommand(() =>
+            {
+                _model.EraseEquation();
+                UpdateButtons();
+            });
         }
 
-        private bool WrongInputValidation(string input)
+        private void UpdateButtons()
         {
-            switch (input)
+            RaisePropertyChanged(nameof(CanPrintMinus));
+            RaisePropertyChanged(nameof(CanPrintDot));
+            RaisePropertyChanged(nameof(CanPrintOperator));
+            RaisePropertyChanged(nameof(CanPrintCloseBracket));
+            RaisePropertyChanged(nameof(CanPrintOpenBracket));
+        }
+
+        private bool CantPrintDot()
+        {
+            if (Equation.Length == 0 ||
+                Equation[Equation.Length - 1] < '0' || Equation[Equation.Length - 1] > '9' ||
+                Equation.Contains("NaN"))
             {
-                case "+":
-                case "*":
-                case "/":
-                    return Equation.Length == 0 ||
-                           Equation[Equation.Length - 1] == '(' ||
-                           Operators.Contains(Equation[Equation.Length - 1].ToString()) ||
-                           Equation.Contains("NaN");
-                case "-":
-                    return Equation.Length != 0 && Operators.Contains(Equation[Equation.Length - 1].ToString());
-                case ",":
-                    if (Equation.Length == 0 ||
-                        Equation[Equation.Length - 1] < '0' || Equation[Equation.Length - 1] > '9' ||
-                        Equation.Contains("NaN"))
-                    {
-                        return true;
-                    }
-
-                    for (int i = Equation.Length - 1; i >= 0; i--)
-                    {
-                        if (Equation[i] == ',')
-                        {
-                            return true;
-                        }
-
-                        if (Equation[i] < '0' || Equation[i] > '9')
-                        {
-                            return false;
-                        }
-                    }
-
-                    return false;
-                case "(":
-                    return Equation.Length != 0 && Equation[Equation.Length - 1] == ',';
-                default:
-                    return false;
+                return true;
             }
+
+            for (int i = Equation.Length - 1; i >= 0; i--)
+            {
+                if (Equation[i] == ',')
+                {
+                    return true;
+                }
+
+                if (Equation[i] < '0' || Equation[i] > '9')
+                {
+                    return false;
+                }
+            }
+
+            return false;
         }
     }
 }
